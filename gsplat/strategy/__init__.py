ca9bd85d056326e2461c5fa87d8af1e4a9525399
Sicheng Li <jasonlisicheng@163.com>
2025-06-23 19:52:45 +0800

    feat: implement freetimegs In this commit, I implement the model and heuristics Gaussian sampling strategy.

--- gsplat/strategy/__init__.py
+++ gsplat/strategy/__init__.py
@@ -3,3 +3,4 @@ from .default import DefaultStrategy
 from .mcmc import MCMCStrategy
 from .STG_Strategy import STG_Strategy
 from .modified_stg import Modified_STG_Strategy
+from .freetimegs import FreeTimeGS_Strategy
